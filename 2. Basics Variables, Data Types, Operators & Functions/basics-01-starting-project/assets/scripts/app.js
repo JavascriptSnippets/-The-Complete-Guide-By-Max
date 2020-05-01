6f5037f47c6c193cc5d02d6bc198d291b9f4a93b
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-05-01 19:47:32 +0300

    call add function when + button is pressed

--- 2. Basics Variables, Data Types, Operators & Functions/basics-01-starting-project/assets/scripts/app.js
+++ 2. Basics Variables, Data Types, Operators & Functions/basics-01-starting-project/assets/scripts/app.js
@@ -1,20 +1,9 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+addBtn.addEventListener('click', add);
 
-// we can add line breaks to string surrounded by Backticks 
-let currnetCalculation = `let me have 
-
-white spaces`;
-
-// to add line breaks to normal string, we need to use back slash
-currnetCalculation = "let me have \n white spaces";
-
-outputResult(currentResult, currnetCalculation);
-
-function add(num1, num2) {
-    const result = num1 + num2;
-    alert(result);
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
-
-add(1, 2);
