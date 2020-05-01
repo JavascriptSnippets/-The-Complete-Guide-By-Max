0d86117a8ccda7f602bf27fefc52efa75c809679
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-05-01 03:54:20 +0300

    add strings

--- 2. Basics Variables, Data Types, Operators & Functions/basics-01-starting-project/assets/scripts/app.js
+++ 2. Basics Variables, Data Types, Operators & Functions/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,20 @@
-alert('this works');
+let currentResult = 0;
+
+currentResult = currentResult + 10;
+
+// we can add line breaks to string surrounded by Backticks 
+let currnetCalculation = `let me have 
+
+white spaces`;
+
+// to add line breaks to normal string, we need to use back slash
+currnetCalculation = "let me have \n white spaces";
+
+outputResult(currentResult, currnetCalculation);
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert(result);
+}
+
+add(1, 2);
