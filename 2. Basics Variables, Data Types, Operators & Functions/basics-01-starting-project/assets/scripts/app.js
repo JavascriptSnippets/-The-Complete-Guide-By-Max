60d45182d0d90f447340aa6df3e52c13e0a6c1a0
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-05-03 23:09:58 +0300

    log operation using a javascript object into console

--- 2. Basics Variables, Data Types, Operators & Functions/basics-01-starting-project/assets/scripts/app.js
+++ 2. Basics Variables, Data Types, Operators & Functions/basics-01-starting-project/assets/scripts/app.js
@@ -7,38 +7,58 @@ multiplyBtn.addEventListener("click", multiply);
 divideBtn.addEventListener("click", divide);
 
 function getUserNumberEntered() {
-    return parseInt(userInput.value);
+  return parseInt(userInput.value);
 }
 
 function add() {
-    const enertedNumber = getUserNumberEntered();
-    const initialResult = currentResult;
-    currentResult += enertedNumber;
-    createAndWriteOutput("+", initialResult, enertedNumber);
+  const enertedNumber = getUserNumberEntered();
+  const initialResult = currentResult;
+  currentResult += enertedNumber;
+  createAndWriteOutput("+", initialResult, enertedNumber);
+  writeToLog("ADD", initialResult, enertedNumber, currentResult);
 }
 
 function multiply() {
-    const enertedNumber = getUserNumberEntered();
-    const initialResult = currentResult;
-    currentResult *= enertedNumber;
-    createAndWriteOutput("*", initialResult, enertedNumber);
+  const enertedNumber = getUserNumberEntered();
+  const initialResult = currentResult;
+  currentResult *= enertedNumber;
+  createAndWriteOutput("*", initialResult, enertedNumber);
+  writeToLog("MULTIPLY", initialResult, enertedNumber, currentResult);
 }
 
 function subtract() {
-    const enertedNumber = getUserNumberEntered();
-    const initialResult = currentResult;
-    currentResult -= enertedNumber;
-    createAndWriteOutput("-", initialResult, enertedNumber);
+  const enertedNumber = getUserNumberEntered();
+  const initialResult = currentResult;
+  currentResult -= enertedNumber;
+  createAndWriteOutput("-", initialResult, enertedNumber);
+  writeToLog("SUBTRACT", initialResult, enertedNumber, currentResult);
 }
 
 function divide() {
-    const enertedNumber = getUserNumberEntered();
-    const initialResult = currentResult;
-    currentResult /= enertedNumber;
-    createAndWriteOutput("/", initialResult, enertedNumber);
+  const enertedNumber = getUserNumberEntered();
+  const initialResult = currentResult;
+  currentResult /= enertedNumber;
+  createAndWriteOutput("/", initialResult, enertedNumber);
+  writeToLog("DIVIDE", initialResult, enertedNumber, currentResult);
 }
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+
+  console.log(logEntry);
 }
